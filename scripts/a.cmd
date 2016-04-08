693d4d834a7e9935193bb123518ba488e371b4df
Sushil Baid <sbaid@microsoft.com>
2016-04-08 13:19:08 +0530

    Fixed script path to full path

--- scripts/a.cmd
+++ scripts/a.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-
+echo %~f1
 if  not "%tools%" == "" (
     @echo hi
     @echo hi2
--- scripts/regCmdAutoRun.cmd
+++ scripts/regCmdAutoRun.cmd
@@ -1,7 +1,8 @@
 @rem This script can be used to register a script as AutoRun script in command prompt shell
 @if "%1"=="" goto :Usage
-    
-@reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d %1 /f 
+
+@echo Registering %~f1 for AutoRun.    
+@reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d %~f1 /f 
 @goto :exit
 :Usage
 @echo %0 ^<path-to-your-auto-run-cmd-file^>
