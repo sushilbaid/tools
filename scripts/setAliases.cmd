f010a37bef0f60ab0cac72e06250cd3b4c13d229
Sushil Baid <sbaid@microsoft.com>
2017-02-03 22:41:40 +0530

    updates

--- scripts/setAliases.cmd
+++ scripts/setAliases.cmd
@@ -4,30 +4,28 @@
 @rem na - macro to edit aliases
 @rem ra - macro to run this script file again in the command shell to update macros
 @echo off
-@setlocal
+@setlocal  
 doskey n=notepad $*
 doskey dm=doskey /macros
-doskey tw=cd /d "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow"
-doskey cc=cd /d "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Team Tools\Dynamic Code Coverage Tools"
+doskey tw=cd /d "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow"
+doskey cc=cd /d "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Team Tools\Dynamic Code Coverage Tools"
 
 @rem set tools env variable to enable these macros
 @rem e.g. tools=d:\g\tools\scripts
-if  not %tools% == "" (
-    doskey na=notepad %tools%\scripts\setAliases.cmd
-    doskey ra=%tools%\scripts\setAliases.cmd
-    doskey dt=cd /d %tools%
-)
+set tools=%~dp0\..\
+doskey na=notepad %tools%\scripts\setAliases.cmd
+doskey ra=%tools%\scripts\setAliases.cmd
+doskey dt=cd /d %tools%
 
 @rem To enable pricate macros - 1) set privateTools env variable 2) place script file at %privateTools%\scripts\setAliases.cmd
 @rem this will enable to checkin private macros in private repo
 @rem e.g. privateTools=d:\g\privateTools
-if  not "%privateTools%" == "" if exist "%privateTools%\scripts\setAliases.cmd" (
-        doskey npa=notepad %privateTools%\scripts\setAliases.cmd
-        doskey rpa=%privateTools%\scripts\setAliases.cmd
-        doskey dpt=cd /d %privateTools%
-        call %privateTools%\scripts\setAliases.cmd
+
+set privatetools=%tools%\..\privatetools
+if exist "%tools%\..\privatetools\scripts\setAliases.cmd"  (
+    doskey npa=notepad %privateTools%\scripts\setAliases.cmd
+    doskey rpa=%privateTools%\scripts\setAliases.cmd
+    doskey dpt=cd /d %privateTools%
+    rem this is not working!!
+    rem call %privateTools%\scripts\setAliases.cmd
 )
-
-
-
-
